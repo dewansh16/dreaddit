f8d9fff5eb45772b631c598d59643eca147cc1b1
Dewansh Patle <dewanshpatle69@gmail.com>
2024-04-21 17:31:05 +0530

    added editor.js and editor show for posts

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import { FC, ReactNode } from "react";
 
 interface ProviderProps {
@@ -11,7 +12,9 @@ const Providers: FC<ProviderProps> = ({ children }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
   );
 };
 
